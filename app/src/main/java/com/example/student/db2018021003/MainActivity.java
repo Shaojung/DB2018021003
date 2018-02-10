52c5dfc0c55a4164a762a28151c2795608d9ef60
Shaojung <shaojung@gmail.com>
2018-02-10 11:17:27 +0800

    ListView Click

--- app/src/main/java/com/example/student/db2018021003/MainActivity.java
+++ app/src/main/java/com/example/student/db2018021003/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.student.db2018021003;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -21,9 +24,22 @@ public class MainActivity extends AppCompatActivity {
         mydata.add("西瓜");
         mydata.add("BB");
         mydata.add("CC");
+        mydata.add("AA2");
+        mydata.add("Apple2");
+        mydata.add("橘子2");
+        mydata.add("西瓜2");
+        mydata.add("BB2");
+        mydata.add("CC2");
         lv = (ListView) findViewById(R.id.listView);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
         android.R.layout.simple_list_item_1, mydata);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, mydata.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         lv.setAdapter(adapter);
 
